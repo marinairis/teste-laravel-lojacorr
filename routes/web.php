1b94867589fae97debb6c8516d1d38d49f1461bd
marina iris <marinairis11@gmail.com>
2023-01-02 13:42:53 -0300

    atualiza readme

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HousesController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,41 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('resolucaoProblema', function() {
+    $casas = [
+        10000,
+        50000,
+        30,
+        30000,
+        10,
+        50,
+        75000,
+    ];
+
+    usort($casas, function($a, $b){
+        if($a > $b) return 1;
+        return -1;
+    });
+
+    $orcamento = 72050;
+    $orcamentoAtual = $orcamento;
+    $casasAdquiridas = 0;
+
+    while( count($casas) > 0 && $orcamentoAtual - $casas[0] > 0) {
+        $casaAtual = $casas[0];
+        if($orcamentoAtual - $casaAtual > 0) {
+            $orcamentoAtual -= $casaAtual;
+            $casasAdquiridas++;
+        }
+        array_shift($casas);
+    }
+
+    $diferencaOrcamento = $orcamento - $orcamentoAtual;
+    echo (
+        "\nOrcamento restante: {$orcamentoAtual}
+        \nCasas adquiridas: {$casasAdquiridas}
+        \nOrcamento utilizado: {$diferencaOrcamento}"
+    );
 });
