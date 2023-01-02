2c63ac9dccebf527c9e1a01234c332b78908cb02
marina iris <marinairis11@gmail.com>
2023-01-01 23:06:52 -0300

    finaliza teste do laravel

--- routes/api.php
+++ routes/api.php
@@ -4,10 +4,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CategoriesController;
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
-
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
